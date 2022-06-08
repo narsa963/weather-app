a36cb8ac71fa3bcd4235086aa52b374a01cbb12e
Ajay Nara <66849552+narsa963@users.noreply.github.com>
2022-06-07 21:12:52 -0400

    changes

--- app.js
+++ app.js
@@ -1,12 +1,31 @@
 const express=require('express');
 const bodyParser=require('body-parser');
 const https=require('https');
-
+const books = [
+  {id: 1 , name : "test1"},
+  {id: 2 , name : "test2"}
+]
 const app=express();
 app.use(bodyParser.urlencoded({extended:true}));
 
-app.get('/', function(req, res){
-  res.sendFile(__dirname +'/index.html');
+app.get('/books', function(req, res){
+  res.json(books);
+  res.send();
+})
+
+app.post('/add', bodyParser.json(),function(req,res){
+  const bookname = req.body;
+  books.push(bookname);
+  res.json(books);
+  console.log(bookname);
+  res.send();
+})
+app.delete('/books/:id', function(req, res){
+  console.log(books);
+  const  delteBooks= books.filter(b =>b.id !=req.params.id );
+  console.log(req.params.id);
+  res.json(delteBooks);
+  res.send();
 })
 
 app.post('/', function(req, res){
@@ -23,9 +42,11 @@ app.post('/', function(req, res){
       const weatherDesc= weatherData.weather[0].description;
       const icon =weatherData.weather[0].icon;
       const image='http://openweathermap.org/img/wn/'+icon+'@2x.png';
-      res.write('<p>weather description' +weatherDesc+'</p>');
-      res.write('<h1>Current temp '+query+'is '+tem+'</h1>');
-      res.write('<img src='+image+' >')
+      //res.write('<p>weather description' +weatherDesc+'</p>');
+      //res.write('<h1>Current temp '+query+'is '+tem+'</h1>');
+      //res.write('<img src='+image+' >')
+      //res.write(weatherData);
+      res.json(weatherData)
       res.send();
     })
   })
---
+++ books.js
@@ -0,0 +1,14 @@
+const books =[
+  {
+    id:1,
+    name:"test1"
+  },
+  {
+    id:2,
+    name:"test2"
+  },
+  {
+    id:3,
+    name:"test3"
+  }
+]
